a588c5577c19497b24d5ef01468330dfd06d9b61
Thomas8008 <ton-email@example.com>
2025-12-11 16:13:43 +0100

    Remove image preload links and fix script.min.js syntax

--- js/aos.js
+++ js/aos.js
@@ -1,32 +1,9 @@
 // Init AOS
-AOS.init({
-  duration: 900,
-  easing: 'ease-out-quart',
-  once: true,
-  offset: 80
-});
-
-// Swiper Main
-const mainSwiper = new Swiper('.swiper-main', {
-  loop: true,
-  spaceBetween: 50,
-  slidesPerView: 1,
-  centeredSlides: true,
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  keyboard: true,
-});
-
-
-
-
-  
+if (typeof AOS !== 'undefined') {
+  AOS.init({
+    duration: 900,
+    easing: 'ease-out-quart',
+    once: true,
+    offset: 80
+  });
+}
